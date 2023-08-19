a70834869d6ca6817f115f8e510c3ab94220bd2a
Łukasz Filipiuk <luke.filipiuk@gmail.com>
2023-08-19 17:26:33 +0200

    feat: Integrate chrome-aws-lambda with Puppeteer for PDF generation

--- pages/api/pdf.ts
+++ pages/api/pdf.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import markdownIt from 'markdown-it';
-import puppeteer from 'puppeteer';
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
 import path from 'path';
 import fs from 'fs';
 
 const md = markdownIt();
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+const handler =  async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'POST') {
-        return res.status(405).json({ message: 'Only POST requests allowed' })
+        return res.status(405).json({ message: 'Only POST requests allowed' });
     }
 
     const html = md.render(req.body.text);
@@ -23,11 +24,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         ${html}
     `;
 
+    let browser = null;
     try {
-        const browser = await puppeteer.launch();
+        // Here's the change. We're setting up Puppeteer to use the Chrome binary from chrome-aws-lambda
+        browser = await puppeteer.launch({
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+        });
+
         const page = await browser.newPage();
         await page.setContent(fullHtml);
-        const pdf = await page.pdf({ format: 'Letter' });
+        const pdf = await page.pdf({ format: 'letter' });
 
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename=cv.pdf');
@@ -36,5 +44,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     } catch (e) {
         console.error(e);
         return res.status(500).json({ message: 'Internal Server Error' });
+    } finally {
+        // Make sure to always close the browser
+        if (browser) {
+            await browser.close();
+        }
     }
-}
+};
+
+export default handler;
